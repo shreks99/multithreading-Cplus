cef085a458e4594277e3a9a7a2819518e59b142c
shreks99 <shreyk1999@gmail.com>
2024-05-09 18:37:51 -0700

    try_lock mutex

--- learnMutex.cpp
+++ learnMutex.cpp
@@ -4,6 +4,7 @@
 #include<vector>
 
 std::mutex task_mutex;
+using namespace std::literals;
 
 void task(const std::string& str) {
     for(int i=0;i<5;i++) {
@@ -12,14 +13,34 @@ void task(const std::string& str) {
        task_mutex.unlock();
     }
 }
-int main() {
-    std::thread thr1(task,"abc");
-    std::thread thr2(task,"def");
-    std::thread thr3(task,"xyz");
+void task1() {
+    std::cout<< "Task1 trying to lock the mutex"<< std::endl;
+    task_mutex.lock();
+    std::cout <<"Task 1 locked the mutex" << std::endl;
+    std::this_thread::sleep_for(500ms);
+    std::cout<< "Task1 is unlocking the mutex"<<std::endl;
+    task_mutex.unlock();
+}
+void task2() {
+    std::this_thread::sleep_for(100ms);
+    std::cout<< "Task2 trying to lock the mutex"<< std::endl;
+    while(!task_mutex.try_lock()) {
+        std::cout<< "Task2 could not lock the mutex"<< std::endl;
+        std::this_thread::sleep_for(100ms);
 
+    }
+    std::cout <<"Task 2 locked the mutex" << std::endl;
+    task_mutex.unlock();
+}
+int main() {
+    // std::thread thr1(task,"abc");
+    // std::thread thr2(task,"def");
+    // std::thread thr3(task,"xyz");
+    std::thread thr1(task1);
+    std::thread thr2(task2);
     thr1.join();
     thr2.join();
-    thr3.join();
+    //thr3.join();
     return 0;
 
 }
